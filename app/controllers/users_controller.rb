aea4c2c8a391f97e9bfc67cd9c679a48c4068248
Abhimanyu <abhi@manyu.in>
2013-01-31 08:17:39 +0530

    Add error sign up page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-  def new
-  end
-
+ 
   def show
   	@user=User.find(params[:id])
+  end
+
+  def new
+  	@user= User.new
+  end
+
+  def create
+  	@user = User.new (params[:user])
+  	if @user.save
+  		#Do something
+  	else
+  		render 'new'
+  	end
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,18 +4,41 @@ describe "User pages" do
 
   subject { page }
 
-  describe "signup page" do
+  describe "signup" do
     before { visit signup_path }
 
-    it { should have_selector('h2',    text: 'Sign Up') }
-    it { should have_selector('title', text: 'Sign Up | Trackr') }
+    let(:submit) {"Signup"}
+
+    describe "with invalid info" do
+        it "should not create a user" do
+            expect {click_button submit}.not_to change(User, :count)
+        end
+    end
+
+    describe "with valid info" do
+        before do
+            fill_in "Name",             with: "Example User"
+            fill_in "Email",            with: "example@manyu.in"
+            fill_in "Password",         with: "foobar"
+            fill_in "Confirmation",     with: "foobar"
+            fill_in "Phone",            with: "+1 (12) 234 23"
+            fill_in "Identifier",       with: "amanyu"       
+        end
+
+        it "should create a user" do
+            expect {click_button submit}.to change(User, :count).by(1)
+        end
+    end
+    
   end
 
   describe "profile page" do
-  	let(:user) {FactoryGirl.create(:user)}
-  	before { visit user_path(user)}
+    let(:user) {FactoryGirl.create(:user)}
+    before { visit user_path(user)}
 
-  	it {should have_selector('h1', text: user.name)}
-  	it {should have_selector('title', text: user.name)}
+    it {should have_selector('h1', text: user.name)}
+    it {should have_selector('title', text: user.name)}
   end
+
+
 end
