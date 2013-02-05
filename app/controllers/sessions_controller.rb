6ddbdfc6c0f2d2ebf854edd9a1bc584fdeffe7af
Abhimanyu <abhi@manyu.in>
2013-02-06 02:36:53 +0530

    Add new controller for sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,13 @@
 class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        render 'new'
+    end
+
+    def destroy
+    end
+
 end
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,7 +7,27 @@ describe "Authentication" do
   describe "signin page" do
     before { visit signin_path }
 
-    it { should have_selector('h1',    text: 'Sign In') }
+    it { should have_selector('h2',    text: 'Sign In') }
     it { should have_selector('title', text: 'Sign In') }
+
+    describe "with invalid information" do
+    	before { click_button "Sign In"}
+
+    	it {should have_selector('title', text:'Sign In')}
+    	it {should have_selector('div.alert.alert-error', text:'Invalid')}
+    end
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
+
+      it { should have_selector('title', text: user.name) }
+      it { should have_link('Sign Out', href: signout_path) }
+      it { should_not have_link('Sign In', href: signin_path) }
+    end
   end
 end
