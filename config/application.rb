00784496a46e6dabdc9fd530fc2af52d68ab6f10
Abhimanyu <abhi@manyu.in>
2013-02-07 23:56:10 +0530

    Try adding custom 404 pages

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module Trackr
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.exceptions_app = self.routes
   end
 end
