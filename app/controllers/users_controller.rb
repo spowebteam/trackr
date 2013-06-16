7a34a71f994e0ab79c8ddb30c2baa673d074f54b
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-06-17 03:48:33 +0530

    Set proper routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -75,21 +75,10 @@ class UsersController < ApplicationController
   end
 
   private
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end  
-    end
-
     def correct_user
       @user=User.find(params[:id])
 
       redirect_to root_path, error:"Access Denied" unless current_user?(@user)
-    end
-
-     def admin_user
-      redirect_to(root_path) unless current_user.admin?
     end
 
 end
