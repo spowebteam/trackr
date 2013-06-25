e7c0fc90bb9a71196e5bfc89a208301272e57fd5
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-06-26 02:36:21 +0530

    Add deploy configuration

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/guru/apps/blog/trackr"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.trackr.sock"
+worker_processes 12
+timeout 30
